a48be604c17e2d5c16cff64bacd63037841dca0a
jerry <645592938@qq.com>
2019-06-13 18:55:16 +0800

    add task swiper

--- pages/task/task.js
+++ pages/task/task.js
@@ -5,7 +5,7 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    currentTab: 0
   },
 
   /**
@@ -62,5 +62,23 @@ Page({
    */
   onShareAppMessage: function () {
 
+  },
+
+  //滑动切换
+  swiperTab: function (e) {
+    this.setData({
+      currentTab: e.detail.current
+    });
+  },
+
+  //点击切换
+  clickTab: function (e) {
+    if (this.data.currentTab === e.target.dataset.current) {
+      return false;
+    } else {
+      this.setData({
+        currentTab: e.target.dataset.current
+      })
+    }
   }
 })
