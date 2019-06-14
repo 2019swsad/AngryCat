cc54e47f8e0cbd972cc1cdfcc96fe753d47ba365
stwie2k <269088185@qq.com>
2019-06-14 21:39:13 +0800

    modify task data

--- login/login.js
+++ login/login.js
@@ -1,27 +1,11 @@
-// pages/taskdetail/taskdetail.js
+// login/login.js
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    "_id": "5cfe0688d7f67f119cbd4967",
-    "title": "Carl's task",
-    "type": "Questionaire",
-    "salary": 20,
-    "description": "Hello World",
-    "beginTime": "2019-08-19T16:00:00.000Z",
-    "expireTime": "2019-08-21T16:00:00.000Z",
-    "participantNum": 1,
-    "tags": "Testing",
-    "uid": "d06146e7-aaff-47a8-831b-99bcf73e1f55",
-    "tid": "17eeefdb-96db-4da4-9d52-2be8c04131b4",
-    "status": "start",
-    "totalCost": 20,
-    "createTime": "2019-06-10 15:28:08",
-    "currentParticipator": 0,
-    "isOrganizer": false,
-    "userinfo": {}
+
   },
 
   /**
