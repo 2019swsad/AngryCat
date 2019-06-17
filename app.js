04c87f322c0930dd808f785c956370e7e7f3f21d
Carlsu <mrpuresu@gmail.com>
2019-06-18 00:51:48 +0800

    根据新的报名接口进行调整
    - 移除POST的Status值
    - 新增报名状态返回值的Toast

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ App({
 
     wx.request({
       url: 'https://www.volley99.com/users/login',
-      data: { "username": "yztest", "password": "123" },
+      data: { "username": "Carl3", "password": "123" },
       method: 'POST', // OPTIONS, GET, HEAD, POST, PUT, DELETE, TRACE, CONNECT
       header: {
         'content-type': 'application/json'
