461661141fd30b0f73f706e9ae65650dc51b355e
stwie2k <269088185@qq.com>
2019-06-17 22:11:23 +0800

    add global username

--- app.js
+++ app.js
@@ -46,7 +46,36 @@ App({
       }
     })
 
-    
+    var self=this;
+
+    wx.request({
+      url: 'https://www.volley99.com/users/self',
+      method: 'GET', // OPTIONS, GET, HEAD, POST, PUT, DELETE, TRACE, CONNECT
+      header: {
+        'content-type': 'application/json',
+        'cookie': wx.getStorageSync("sessionId")
+      }, // 设置请求的 header
+      success: function (res) {
+
+        if (res.statusCode == 200) {
+
+          self.globalData.username=res.data.username;
+         
+
+
+          console.log(res.data);
+        } else {
+          console.log("index.js wx.request CheckCallUser statusCode" + res.statusCode);
+        }
+      },
+      fail: function () {
+        console.log("index.js wx.request CheckCallUser fail");
+      },
+      complete: function () {
+        // complete
+      }
+    })
+
 
 
     // 获取用户信息
