7d0e6f0d64044a35fcec971977b6b6883eff840b
stwie2k <269088185@qq.com>
2019-06-18 12:00:07 +0800

    modify orderlist

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ App({
 
     wx.request({
       url: 'https://www.volley99.com/users/login',
-      data: { "username": "Carl4", "password": "123" },
+      data: { "username": "yztest", "password": "123" },
       method: 'POST', // OPTIONS, GET, HEAD, POST, PUT, DELETE, TRACE, CONNECT
       header: {
         'content-type': 'application/json'
